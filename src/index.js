14a8c29921b6e71df4cd1ba8f03b03f4d9f166cd
RudikAleksandr <rudin_aleksandr@mail.ru>
2017-09-14 00:44:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var stackOpenBrakets = [];
+
+    for (let i = 0, flagBraket = true; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (bracketsConfig[j][0] === str[i] && bracketsConfig[j][0] === bracketsConfig[j][1]) {
+                if (flagBraket) {
+                    stackOpenBrakets.push(str[i]);
+                    flagBraket = false;
+                    break;
+                }
+                else {
+                    flagBraket = true;
+                    if (stackOpenBrakets.pop() !== bracketsConfig[j][0]) {
+                        return false;
+                    }
+                }
+            }
+            else if (bracketsConfig[j][0] === str[i]) {
+                stackOpenBrakets.push(str[i]);
+                break;
+            }
+            else if (bracketsConfig[j][1] === str[i]) {
+                if (stackOpenBrakets.pop() !== bracketsConfig[j][0]) {
+                    return false;
+                }
+            }
+        }
+    }
+    if (stackOpenBrakets.length != 0) {
+        return false;
+    }
+
+    return true;
 }
